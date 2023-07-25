46e32d32b444fcf3cdb751724d4ed6f7bec16f2f
shubham <shubhamdixit863@gmail.com>
2023-07-25 10:01:52 +0530

    channels intro

--- main.go
+++ main.go
@@ -1,36 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-func Data(w *sync.WaitGroup) {
-	fmt.Println("jejeje")
-	for i := 0; i < 10; i++ {
-		fmt.Println("Running from go routine", i)
-	}
+func Data(ch chan int) {
+	c := 10
 
-	w.Done()
+	ch <- c //  <-ch
+	close(ch)
+
 }
 
 func main() {
+	ch := make(chan int) // unbuffered channel
+	go Data(ch)
 
-	var wg sync.WaitGroup
-
-	go Data(&wg)
-
-	wg.Add(1)
-	fmt.Println("Hello world")
-
-	//time.Sleep(1 * time.Second)  // wrong way
-
-	//fmt.Scanln()   // wrong way
-
-	// Correct way is
-	// channels
-	// waitGroups
-	wg.Wait()
-	fmt.Println("main routine exited")
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 
 }
---
+++ multiplegoroutines/multiple_goroutines.go
@@ -0,0 +1,51 @@
+package multiplegoroutines
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Data(w *sync.WaitGroup) {
+	fmt.Println("jejeje")
+	for i := 0; i < 10; i++ {
+		fmt.Println("Running from go routine", i)
+	}
+
+	w.Done()
+}
+
+func Multiple(wg *sync.WaitGroup) {
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			time.Sleep(2 * time.Second)
+			fmt.Println("The go routine", i)
+			wg.Done()
+		}(i)
+
+	}
+
+}
+
+func main() {
+	var wg sync.WaitGroup
+	//go Data(&wg)
+
+	Multiple(&wg)
+
+	//wg.Add(1)
+	fmt.Println("Hello world")
+
+	//time.Sleep(1 * time.Second)  // wrong way
+
+	//fmt.Scanln()   // wrong way
+
+	// Correct way is
+	// channels
+	// waitGroups
+	wg.Wait()
+	fmt.Println("main routine exited")
+
+}
