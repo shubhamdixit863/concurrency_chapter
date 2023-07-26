fa01bd3ad3f744556f5eb1419581f586bc6078ba
shubham <shubhamdixit863@gmail.com>
2023-07-26 09:56:23 +0530

    channels and go routines ,race condition

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func Data(ch chan int) {
@@ -14,12 +14,42 @@ func Data(ch chan int) {
 
 }
 
+type Person struct {
+	age     int
+	isVoter bool
+}
+
+func SendDer(ch chan<- *Person, wg *sync.WaitGroup) { // send only
+	g := &Person{
+		age:     10,
+		isVoter: true,
+	}
+	ch <- g
+
+	g.isVoter = true
+	g.age = 100
+	defer wg.Done()
+	close(ch)
+
+}
+
+func Receiver(ch <-chan *Person, wg *sync.WaitGroup) { //receive only channel
+	//v := <-ch
+	fmt.Println("Receiver Go routine---", <-ch)
+	defer wg.Done()
+}
+
 func main() {
-	ch := make(chan int)
-	go Data(ch)
-	time.Sleep(5 * time.Second)
-	fmt.Println(<-ch)
-	fmt.Println("Value received")
+	ch := make(chan *Person)
+
+	var wg sync.WaitGroup
+	go Receiver(ch, &wg)
+	go SendDer(ch, &wg)
+
+	wg.Add(2)
+	//time.Sleep(2 * time.Second)
+
+	wg.Wait()
 
 }
 
@@ -27,3 +57,5 @@ func main() {
 // is send a struct over the channel
 // and print out the value of the struct in another routine
 // then exit the main routine
+
+// We can do buubble sort using channels ,select statement and go routines->
