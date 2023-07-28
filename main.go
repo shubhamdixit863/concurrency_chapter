2a2c2efb8e2e26bae9dbadc62f4503480126bdcb
shubham <shubhamdixit863@gmail.com>
2023-07-28 09:16:35 +0530

    Race condition

--- main.go
+++ main.go
@@ -2,66 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func BuubleSort(slc []int) {
-
-	for i := 0; i < len(slc); i++ {
-		// inneer loop
-
-		for j := 0; j < len(slc)-1; j++ {
-			if slc[j] > slc[j+1] {
-				// swap here
-				slc[j], slc[j+1] = slc[j+1], slc[j]
-			}
-		}
-
-	}
-}
-
-func BuubleSortConcurrent(slc []int, ch chan int, exit chan struct{}) {
-
-	for i := 0; i < len(slc); i++ {
-
-		ch <- i
-	}
-
-	exit <- struct{}{}
-}
-
-func InnerLoop(slc []int) {
-	for j := 0; j < len(slc)-1; j++ {
-		if slc[j] > slc[j+1] {
-			// swap here
-			slc[j], slc[j+1] = slc[j+1], slc[j]
-		}
-	}
-
-}
 func main() {
+	var value int
+	var wg sync.WaitGroup
 
-	c := []int{34, 1, 23, 234, 0, 2, 99}
-	exit := make(chan struct{})
-	//BuubleSort(c)
-
-	buubleSortIndex := make(chan int)
-
-	go BuubleSortConcurrent(c, buubleSortIndex, exit)
-
-loop:
-	for {
-		select {
-		case <-buubleSortIndex:
-			InnerLoop(c)
-
-		case <-exit:
-			break loop
-
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			value++
 		}
 
-	}
+	}()
 
-	fmt.Println("Sorted array", c)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			value--
+		}
+
+	}()
+
+	wg.Add(2)
+	fmt.Println(value)
+	wg.Wait()
+
 }
 
 // selection sort algorithm ---->
--- multiplegoroutines/multiple_goroutines.go
+++ multiplegoroutines/multiple_goroutines.go
@@ -177,5 +177,74 @@ loop:
 
 	//fmt.Println("Main finished")
 
+package main
+
+import (
+	"fmt"
+)
+
+func BuubleSort(slc []int) {
+
+	for i := 0; i < len(slc); i++ {
+		// inneer loop
+
+		for j := 0; j < len(slc)-1; j++ {
+			if slc[j] > slc[j+1] {
+				// swap here
+				slc[j], slc[j+1] = slc[j+1], slc[j]
+			}
+		}
+
+	}
+}
+
+func BuubleSortConcurrent(slc []int, ch chan int, exit chan struct{}) {
+
+	for i := 0; i < len(slc); i++ {
+
+		ch <- i
+	}
+
+	exit <- struct{}{}
+}
+
+func InnerLoop(slc []int) {
+	for j := 0; j < len(slc)-1; j++ {
+		if slc[j] > slc[j+1] {
+			// swap here
+			slc[j], slc[j+1] = slc[j+1], slc[j]
+		}
+	}
+
+}
+func main() {
+
+	c := []int{34, 1, 23, 234, 0, 2, 99}
+	exit := make(chan struct{})
+	//BuubleSort(c)
+
+	buubleSortIndex := make(chan int)
+
+	go BuubleSortConcurrent(c, buubleSortIndex, exit)
+
+loop:
+	for {
+		select {
+		case <-buubleSortIndex:
+			InnerLoop(c)
+
+		case <-exit:
+			break loop
+
+		}
+
+	}
+
+	fmt.Println("Sorted array", c)
+}
+
+// selection sort algorithm ---->
+
+
 
 */
