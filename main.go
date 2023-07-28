0b855037b3788411108ceb207fefaf2309ec0f62
shubham <shubhamdixit863@gmail.com>
2023-07-28 09:30:02 +0530

    mutex

--- main.go
+++ main.go
@@ -5,29 +5,60 @@ import (
 	"sync"
 )
 
+var value int = 10
+
 func main() {
-	var value int
+
 	var wg sync.WaitGroup
+	var mutex sync.Mutex
 
 	go func() {
 		defer wg.Done()
-		for i := 0; i < 10; i++ {
+		mutex.Lock()
+		for i := 0; i < 100000; i++ {
+			value++
+		}
+		mutex.Unlock()
+
+	}()
+
+	go func() {
+		defer wg.Done()
+		mutex.Lock()
+		for i := 0; i < 100000; i++ {
 			value++
 		}
 
+		mutex.Unlock()
+
 	}()
 
 	go func() {
 		defer wg.Done()
-		for i := 0; i < 10; i++ {
+		mutex.Lock()
+		for i := 0; i < 100000; i++ {
 			value--
 		}
 
+		mutex.Unlock()
+
 	}()
 
-	wg.Add(2)
-	fmt.Println(value)
+	go func() {
+		defer wg.Done()
+		mutex.Lock()
+
+		for i := 0; i < 100000; i++ {
+			value--
+		}
+		mutex.Unlock()
+
+	}()
+
+	wg.Add(4)
+
 	wg.Wait()
+	fmt.Println(value)
 
 }
 
